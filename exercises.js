7f57d59ee5d6105e5b0e154d4a8630ab991af6ce
kinwail <kinwail@gmail.com>
2018-06-10 23:39:06 -1000

    completed #6

--- exercises.js
+++ exercises.js
@@ -139,6 +139,14 @@ console.log(bigBird('big'))
  * Console.log your result.
 */
 
+function isEqual(first,second){
+  if(first === second){ 
+    return "You look mahvelous!"
+  }else{
+    return "I don't know who you are anymore"
+  }
+}
+console.log(isEqual('great', 'great'))
 
 /*
  * #7
